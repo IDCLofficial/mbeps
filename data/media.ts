c0f9cac9eae6124c6565aa6175dd2a0d9072131e
jephthahdominic <stdominicjephthah63@gmail.com>
2025-08-07 12:01:27 +0200

    Worked on the changes

--- data/media.ts
+++ data/media.ts
@@ -7,18 +7,8 @@ export const Media = [
     isVideo: false
     },
    {
-    image:"/images/homeImage1.jpg",
-    title: "Team reviewing the budgetting standard",
-    isVideo: false
-    },
-   {
     image:"/images/homeImage2.jpg",
     title: "congress meeting",
-    isVideo: false
-    },
-   {
-    image:"/images/homeImage3.jpg",
-    title: "The commissioner being awarded for excellence",
     isVideo: false
     }
 ]
--- src/app/media/MediaGalleryCard.tsx
+++ src/app/media/MediaGalleryCard.tsx
@@ -10,8 +10,8 @@ interface MediaGalleryCardProps {
 const MediaGalleryCard: React.FC<MediaGalleryCardProps> = ({ image, title, isVideo }) => {
   return (
     <div className="bg-white rounded-xl border border-gray-200 shadow-sm overflow-hidden flex flex-col cursor-pointer transition hover:shadow-md">
-      <div className="relative w-full h-48">
-        <Image src={image} alt={title} fill className="object-cover" />
+      <div className="relative w-full h-[300px]">
+        <Image src={image} alt={title} fill className="object-fill" />
         {isVideo && (
           <a href="#" className="absolute inset-0 flex items-center justify-center">
             <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
